661b0941c3cb82dabd6195f93a1910513197b6b4
Aya Salama <asalama0204@gmail.com>
2024-09-29 10:48:28 -0400

    score to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    score: {
+        type: Number,
+        required: false
+    },
 }, {
     timestamps: true
 });
